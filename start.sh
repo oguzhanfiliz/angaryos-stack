4dd9c62e64a689a5e82be94d525a139b39d45c01
Ömer SAVAŞ <omer.savas@hotmail.com.tr>
2020-01-14 09:17:55 +0300

    BugFix

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ cd Angaryos/
 chmod 777 -R frontend/
 chmod 777 -R backend/storage/
 chmod 777 -R backend/bootstrap/cache/
+chmod 0700 -R services/postgresql
 
 docker stack deploy --compose-file ./docker-stack.yml angaryos
