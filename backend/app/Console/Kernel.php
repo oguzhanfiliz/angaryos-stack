ccab291db0d7ebf82e5974678bfd16817d9b574f
karapazar <omer.savas@hotmail.com.tr>
2020-03-09 15:02:18 +0300

    BugFix

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -5,6 +5,8 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+use DB;
+
 class Kernel extends ConsoleKernel
 {
     protected $commands = [ ];
@@ -14,8 +16,17 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         
-        $no = (int)(\DB::table('settings')->where('name', 'REC_COUNT_PER_PAGE')->first()->value);
-        \DB::table('settings')->where('name', 'REC_COUNT_PER_PAGE')->update(['value' => $no+1]);
+        //$no = (int)(\DB::table('settings')->where('name', 'REC_COUNT_PER_PAGE')->first()->value);
+        //\DB::table('settings')->where('name', 'REC_COUNT_PER_PAGE')->update(['value' => $no+1]);
+        
+        $missions = DB::table('missions')->where('state', TRUE)->get();
+        
+        foreach($missions as $mission)
+            if(strlen($mission->cron) > 0)
+                $schedule->call(function () use($mission) 
+                {
+                    eval(helper('clear_php_code', $mission->php_code));  
+                })->cron($mission->cron);
     }
 
     protected function commands()
