1f630b0f50be3b7495afef39fb25c727b08876a9
karapazar <omer.savas@hotmail.com.tr>
2020-03-10 09:14:13 +0300

    BugFix

--- start.sh
+++ start.sh
@@ -1,8 +1,12 @@
 apt-get update
 apt-get install -y docker.io git
+
 docker swarm init
+
 git clone https://github.com/karapazar/Angaryos
+
 cd Angaryos/
+
 chmod 777 -R frontend/
 chmod 777 -R backend/storage/
 chmod 777 -R backend/bootstrap/cache/
