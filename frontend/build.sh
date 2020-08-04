d9295d1e2262a0d1472e11db85493da72495c695
karapazar <omer.savas@hotmail.com.tr>
2020-08-05 00:57:58 +0300

    MinorUpdate

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,3 +1,6 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 nvm install v12.8.0
-ng build -prod
+ng build -prod --optimization=false
+mv ./dist/light/index.aero.html ./dist/light/index.html
+
+#<link rel="stylesheet" href="https://192.168.10.185/assets/themes/aero/assets/css/style.min.css"></head>
