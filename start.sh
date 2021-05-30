8ca707514ba1127e0b9bd8466c7226b5f791ac97
karapazar <omer.savas@hotmail.com.tr>
2021-05-30 12:57:29 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,11 +7,20 @@ git clone https://github.com/MikroGovernment/angaryos-stack.git
 
 cd angaryos-stack/
 
-chmod 755 -R frontend/
-chmod 777 -R backend/storage/
-chmod 777 -R backend/public/
-chmod 777 -R backend/bootstrap/cache/
-chmod 777 -R services/
-chmod 600 services/postgresql/.pgpass
+chmod 755 -R ./frontend/
+chmod 777 -R ./backend/storage/
+chmod 777 -R ./backend/public/
+chmod 777 -R ./backend/bootstrap/cache/
+chmod 777 -R ./services/
+chmod 600 ./services/postgresql/.pgpass
+
+read -p "Enter new host address: "  hostAdress
+
+declare -a arr=("./backend/.env" "./frontend/src/environments/environment.prod.ts" "./frontend/src/environments/environment.ts")
+for i in "${arr[@]}"
+do
+   echo 'replace: '"$i"
+   sed 's/192.168.10.185/'"$hostAdress"'/g' "$i" > ./temp.txt && mv temp.txt "$i"
+done
 
 docker stack deploy --compose-file ./docker-stack.yml angaryos
