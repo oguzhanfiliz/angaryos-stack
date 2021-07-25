8f04547f9f75959d05adb0e031d4605923d8e2dc
karapazar <omer.savas@hotmail.com.tr>
2021-07-25 23:56:58 +0300

    mobilFlutterIlkYukleme

--- mobil/start.sh
+++
@@ -1,4 +0,0 @@
-echo fs.inotify.max_user_watches=524288 | tee -a /etc/sysctl.conf 
-cat /proc/sys/fs/inotify/max_user_watches
-sysctl -p
-ionic serve --lab --address=0.0.0.0 --disableHostCheck --ssl 
